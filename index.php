1057e391b0316fa28a89dac18fd74dba55c868ea
Jakub Karp <karptutaj@gmail.com>
2019-10-23 21:50:16 +0200

    Amen 1

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+$ipAddres=$_SERVER['REMOTE_ADDR'];
+
+ 
 $info = exec("uname -a");
 settype($info, 'string');
 $weight = exec("python3 obecnawaga.py");
@@ -45,6 +48,7 @@ echo <<<EOT
               </header>
               <article>
                 <h2>$weight</h2>
+                <h3>$ipAddres</h3>
                   <p>$info</p>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
